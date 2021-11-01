7e7e71111df737c96d91c77a553ff7fb12ea36e2
9831065 <najafizadeh21@gmail.com>
2021-11-01 20:43:39 +0330

    Author controller full methods, basic parts added

--- authors/app/Http/Controllers/AuthorController.php
+++ authors/app/Http/Controllers/AuthorController.php
@@ -2,15 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
 class AuthorController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json(Author::all());
     }
 
-    public function show($id)
+    public function show($id): JsonResponse
     {
-        //
+        return response()->json(Author::find($id));
+    }
+
+    public function create(Request $request): JsonResponse
+    {
+        $author = Author::create($request->all());
+
+        return response()->json($author, 201);
+    }
+
+    public function update($id, Request $request): JsonResponse
+    {
+        $author = Author::findOrFail($id);
+        $author->update($request->all());
+
+        return response()->json($author, 200);
+    }
+
+    public function delete($id)
+    {
+        Author::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
     }
 }

1c15bc9da321ab77fb6cebf909e41ae2484a1da0
9831065 <najafizadeh21@gmail.com>
2021-11-04 20:47:26 +0330

    Begin a documentation

--- authors/app/Http/Controllers/AuthorController.php
+++ authors/app/Http/Controllers/AuthorController.php
@@ -5,19 +5,45 @@ namespace App\Http\Controllers;
 use App\Models\Author;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Laravel\Lumen\Http\ResponseFactory;
 
+/**
+ * Class AuthorController is a controller for authors.
+ *
+ * @package App\Http\Controllers
+ */
 class AuthorController extends Controller
 {
+    /**
+     * Get all of the authors.
+     *
+     * @return JsonResponse
+     */
     public function index(): JsonResponse
     {
         return response()->json(Author::all());
     }
 
+    /**
+     * Get a single author.
+     *
+     * @param $id
+     * @return JsonResponse
+     */
     public function show($id): JsonResponse
     {
         return response()->json(Author::find($id));
     }
 
+    /**
+     * Storing a new author.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function store(Request $request): JsonResponse
     {
         $this->validate($request, [
@@ -31,6 +57,14 @@ class AuthorController extends Controller
         return response()->json($author, 201);
     }
 
+    /**
+     * Update a author.
+     *
+     * @param $id
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function update($id, Request $request): JsonResponse
     {
         $this->validate($request, [
@@ -44,6 +78,12 @@ class AuthorController extends Controller
         return response()->json($author, 200);
     }
 
+    /**
+     * Destroying an exiting author
+     *
+     * @param $id
+     * @return Response|ResponseFactory
+     */
     public function destroy($id)
     {
         Author::findOrFail($id)->delete();

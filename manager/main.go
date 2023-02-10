256a92cac9f3754517013a9f4cdcadf046e3b126
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-11 02:16:48 +0330

    init: echo

--- manager/main.go
+++ manager/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.POST("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello")
+	})
+
+	e.Logger.Fatal(e.Start(":10021"))
 }

9d6ca705cccc013a13f59e6bd7b649c262ca47c1
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-11 02:21:34 +0330

    add: validation for model

--- manager/main.go
+++ manager/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Model struct {
+	Id    int         `json:"id"`
+	Value interface{} `json:"value"`
+}
+
 func main() {
 	e := echo.New()
 
 	e.POST("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello")
+		var m Model
+
+		if err := c.Bind(&m); err != nil {
+			return err
+		}
+
+		if m.Id < 1 {
+			return errors.New("id field should be positive")
+		}
+
+		return c.String(http.StatusCreated, "OK")
 	})
 
 	e.Logger.Fatal(e.Start(":10021"))

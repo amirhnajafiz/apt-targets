66b1c45e217a7c4975d1419bc0983ce038676387
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-12 12:58:25 +0330

    add: fiber logic

--- storage/main.go
+++ storage/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -9,15 +11,25 @@ type Model struct {
 	StudentNumber string `json:"student_number"`
 }
 
+var storage []Model
+
 func main() {
 	app := fiber.New()
 
 	app.Get("/store", func(ctx *fiber.Ctx) error {
-
+		return ctx.JSON(storage)
 	})
 
 	app.Put("/store", func(ctx *fiber.Ctx) error {
+		var m Model
 
+		if e := ctx.BodyParser(&m); e != nil {
+			return e
+		}
+
+		storage = append(storage, m)
+
+		return ctx.SendStatus(http.StatusCreated)
 	})
 
 	if er := app.Listen(":10020"); er != nil {

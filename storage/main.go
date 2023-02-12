dca5eeba890ba5f5ff94b9632c75f94a491545e8
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-12 12:55:41 +0330

    init: storage

--- storage/main.go
+++ storage/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
+func main() {
+	app := fiber.New()
+
+	app.Get("/store", func(ctx *fiber.Ctx) error {
+
+	})
+
+	app.Put("/store", func(ctx *fiber.Ctx) error {
+
+	})
+
+	if er := app.Listen(":10020"); er != nil {
+		panic(er)
+	}
 }
